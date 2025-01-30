fa1eb0ab6686581699337ddf1f93380b2c09a98a
MD.NASIR UDDIN ANIK <nasiruddinanik54@gmail.com>
2025-01-30 17:01:30 +0600

    hover animation add

--- src/Components/ContactHome.jsx
+++ src/Components/ContactHome.jsx
@@ -1,14 +1,23 @@
 import { FaLocationDot } from "react-icons/fa6";
 import { MdMail } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
+import * as motion from "motion/react-client"
 
 
 const ContactHome = () => {
+
+    const box = {
+        width: 100,
+        height: 100,
+        // backgroundColor: "#9911ff",
+        borderRadius: 5,
+    }
+    
     return (
         <div className="lg:px-16">
             <h3 className="text-4xl font-bold text-center mb-5">Contact Me</h3>
             <div className="grid lg:grid-cols-2 grid-cols-1 gap-6">
-                <div className="bg-gray-300 rounded-xl p-9">
+                <div className="bg-gray-300 rounded-xl shadow-md p-9 hover:shadow-2xl hover:scale-105 transition-transform duration-300">
                     <h3 className="text-xl font-bold">Get in Touch</h3>
                     <div className="divider w-52"></div>
                     <div className="space-y-5">
@@ -18,7 +27,7 @@ const ContactHome = () => {
                         <button className="btn bg-yellow-400 text-white text-xl border-2 border-white">Send Message</button>
                     </div>
                 </div>
-                <div className="bg-blue-200 rounded-xl p-9">
+                <div className="bg-blue-200 rounded-xl shadow-md p-9 hover:shadow-2xl hover:scale-105 transition-transform duration-300">
                     <h3 className="text-xl font-bold">Get in Touch</h3>
                     <div className="divider w-52"></div>
                     <div className="space-y-5">

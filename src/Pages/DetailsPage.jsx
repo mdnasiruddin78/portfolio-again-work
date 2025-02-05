89685a40a0dc6a9e77b20969fa10ebe69c10151c
MD.NASIR UDDIN ANIK <nasiruddinanik54@gmail.com>
2025-02-05 18:07:20 +0600

    link tab add

--- src/Pages/DetailsPage.jsx
+++ src/Pages/DetailsPage.jsx
@@ -11,7 +11,7 @@ const DetailsPage = () => {
     const { image, name, live_link, github_link, technology_use, key_features, challenges, shortcoming_of_the_projects, thinking_and_improving } = details
 
     return (
-        <div className="w-11/12 mx-auto lg:px-16">
+        <div className="w-11/12 mx-auto lg:px-16 bg-base-300">
             <div className="card group bg-lime-100 rounded-md shadow-md">
                 <div className="">
                     <img
@@ -35,13 +35,13 @@ const DetailsPage = () => {
                     <p><span className="font-bold">Shortcoming_of_the_projects:</span> <ul className="text-gray-500 ml-5"><li className="list-disc">{shortcoming_of_the_projects}</li></ul></p>
                     <p><span className="font-bold">Thinking_and_improving:</span> <ul className="text-gray-500 ml-5"><li className="list-disc">{thinking_and_improving}</li></ul></p>
                     <div className="space-x-4">
-                        <a href={live_link}><button className='btn px-7 rounded-full bg-slate-800 text-xl text-white'><FaLink />Live Link</button></a>
-                        <a href={github_link}><button className='btn px-7 rounded-full bg-green-600 text-xl text-white'><IoLogoGithub />
-                        GitHub Link</button></a>
+                        <a href={live_link} target="_blank"><button className='btn px-5 rounded-full bg-slate-800 text-xl text-white'><FaLink />Live Link</button></a>
+                        <a href={github_link} target="_blank"><button className='btn px-5 rounded-full bg-green-600 text-xl text-white'><IoLogoGithub />
+                            GitHub Link</button></a>
                     </div>
                 </div>
             </div>
-            <Link to={'/'} className='btn mt-5 mb-5 rounded-md border-2 border-blue-500 text-blue-400 hover:bg-blue-500 hover:text-white hover:border-blue-500'>Go To Home</Link>
+            <Link to={'/'} className='btn mt-5 mb-5 rounded-md border-[#3F8E00] bg-[#3F8E00] stroke-[#62BA1B] text-white'>Go To Home</Link>
         </div>
     );
 };

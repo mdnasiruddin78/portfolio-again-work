6acd330153f9b5ad0a9d72e3a8115d052257013b
MD.NASIR UDDIN ANIK <nasiruddinanik54@gmail.com>
2025-02-14 12:44:53 +0600

    box color add

--- src/Components/MySkills.jsx
+++ src/Components/MySkills.jsx
@@ -18,55 +18,55 @@ const MySkills = () => {
                 <p className="text-center text-[#959595]">Skills and what i am learning</p>
             </div>
             <div className="grid lg:grid-cols-3 gap-8 md:grid-cols-2 grid-cols-1">
-                <div className="bg-[#1b1b1b] py-16 rounded-xl" data-aos="fade-up" data-aos-duration="3000">
+                <div className="bg-[#1b1b1b] py-16 rounded-xl border-2 border-white" data-aos="fade-up" data-aos-duration="3000">
                     <div className="flex justify-center">
                         <ImHtmlFive2 className="text-[#FD6F00] text-7xl text-center" />
                     </div>
                     <p className="text-[#959595] text-xl font-bold text-center">HTML</p>
                 </div>
-                <div className="bg-[#1b1b1b] py-16 rounded-xl" data-aos="fade-up" data-aos-duration="3000">
+                <div className="bg-[#1b1b1b] py-16 rounded-xl border-2 border-white" data-aos="fade-up" data-aos-duration="3000">
                     <div className="flex justify-center">
                         <SiCss3 className="text-[#FD6F00] text-7xl text-center" />
                     </div>
                     <p className="text-[#959595] text-xl font-bold text-center">CSS</p>
                 </div>
-                <div className="bg-[#1b1b1b] py-16 rounded-xl" data-aos="fade-up" data-aos-duration="3000">
+                <div className="bg-[#1b1b1b] py-16 rounded-xl border-2 border-white" data-aos="fade-up" data-aos-duration="3000">
                     <div className="flex justify-center">
                         <RiTailwindCssFill className="text-[#FD6F00] text-7xl text-center" />
                     </div>
                     <p className="text-[#959595] text-xl font-bold text-center">TAILWIND CSS</p>
                 </div>
-                <div className="bg-[#1b1b1b] py-16 rounded-xl" data-aos="fade-up" data-aos-duration="3000">
+                <div className="bg-[#1b1b1b] py-16 rounded-xl border-2 border-white" data-aos="fade-up" data-aos-duration="3000">
                     <div className="flex justify-center">
                         <IoLogoFirebase className="text-[#FD6F00] text-7xl text-center" />
                     </div>
                     <p className="text-[#959595] text-xl font-bold text-center">FIREBASE</p>
                 </div>
-                <div className="bg-[#1b1b1b] py-16 rounded-xl" data-aos="fade-up" data-aos-duration="3000">
+                <div className="bg-[#1b1b1b] py-16 rounded-xl border-2 border-white" data-aos="fade-up" data-aos-duration="3000">
                     <div className="flex justify-center">
                         <IoLogoJavascript className="text-[#FD6F00] text-7xl text-center" />
                     </div>
                     <p className="text-[#959595] text-xl font-bold text-center">JAVASCRIPT</p>
                 </div>
-                <div className="bg-[#1b1b1b] py-16 rounded-xl" data-aos="fade-up" data-aos-duration="3000">
+                <div className="bg-[#1b1b1b] py-16 rounded-xl border-2 border-white" data-aos="fade-up" data-aos-duration="3000">
                     <div className="flex justify-center">
                         <FaReact className="text-[#FD6F00] text-7xl text-center" />
                     </div>
                     <p className="text-[#959595] text-xl font-bold text-center">REACT.JS</p>
                 </div>
-                <div className="bg-[#1b1b1b] py-16 rounded-xl" data-aos="fade-up" data-aos-duration="3000">
+                <div className="bg-[#1b1b1b] py-16 rounded-xl border-2 border-white" data-aos="fade-up" data-aos-duration="3000">
                     <div className="flex justify-center">
                         <FaNodeJs className="text-[#FD6F00] text-7xl text-center" />
                     </div>
                     <p className="text-[#959595] text-xl font-bold text-center">NODE.JS</p>
                 </div>
-                <div className="bg-[#1b1b1b] py-16 rounded-xl" data-aos="fade-up" data-aos-duration="3000">
+                <div className="bg-[#1b1b1b] py-16 rounded-xl border-2 border-white" data-aos="fade-up" data-aos-duration="3000">
                     <div className="flex justify-center">
                         <SiMongodb className="text-[#FD6F00] text-7xl text-center" />
                     </div>
                     <p className="text-[#959595] text-xl font-bold text-center">MONGODB</p>
                 </div>
-                <div className="bg-[#1b1b1b] py-16 rounded-xl" data-aos="fade-up" data-aos-duration="3000">
+                <div className="bg-[#1b1b1b] py-16 rounded-xl border-2 border-white" data-aos="fade-up" data-aos-duration="3000">
                     <div className="flex justify-center">
                         <SiExpress className="text-[#FD6F00] text-7xl text-center" />
                     </div>
